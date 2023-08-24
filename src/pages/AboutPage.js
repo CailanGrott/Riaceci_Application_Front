9ec2fff895d6a3aba5f1df30ee3afbf9c09361ce
CailanGrott <cailangrott@gmail.com>
2023-08-23 22:33:09 -0300

    first commit

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/AboutPage.css';
+
+function AboutPage() {
+    return (
+        <div className="about-container">
+            <h1>About Page</h1>
+            <p>Sobre nós...</p>
+        </div>
+    );
+}
+
+export default AboutPage;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/HomePage.css';
+
+function HomePage() {
+    return (
+        <div className="home-container">
+            <h1>Home Page</h1>
+            <p>Bem-vindo à página inicial.</p>
+        </div>
+    );
+}
+
+export default HomePage;
